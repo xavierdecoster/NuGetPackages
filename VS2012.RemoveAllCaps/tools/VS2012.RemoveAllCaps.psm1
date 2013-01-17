cee1983419f4235d6097060241d5e7a996604c01
Xavier Decoster <xavier.decoster@gmail.com>
2013-01-17 13:32:19 +0100

    Update VS2012.RemoveAllCaps/tools/VS2012.RemoveAllCaps.psm1
    Edited restart notification message

--- VS2012.RemoveAllCaps/tools/VS2012.RemoveAllCaps.psm1
+++ VS2012.RemoveAllCaps/tools/VS2012.RemoveAllCaps.psm1
@@ -10,6 +10,6 @@ function Set-AllCaps ($value) {
 	$root = $DTE.RegistryRoot
 	$path = "HKCU:\$root\General"
 	Set-ItemProperty -Path $path -Name SuppressUppercaseConversion -Type DWord -Value $value
-	Write-Host "Please restart Visual Studio to take effect"
+	Write-Host "Please restart Visual Studio for changes to take effect."
 }
 
